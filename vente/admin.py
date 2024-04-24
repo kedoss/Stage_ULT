9c28976e278a2fe60f3b7f6f766d420e568985b2
kedoss <kedarndicungue6@gmail.com>
2024-04-24 21:49:19 +0200

    commit ya kane

--- vente/admin.py
+++ vente/admin.py
@@ -3,3 +3,7 @@ from .models import*
 # Register your models here.
 #admin.site.register(category)
 admin.site.register(produit)
+admin.site.register(stock)
+admin.site.register(commande)
+admin.site.register(ProduitCommande)
+admin.site.register(Paiement)
